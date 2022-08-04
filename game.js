cb6f12804192ed1b44eb1472b755f3fd954f88e6
Nikolas Furtado <nikolasfurtado@gmail.com>
2022-08-03 23:02:08 -0400

    user and pattern logic working

--- game.js
+++ game.js
@@ -1,40 +1,21 @@
-//document.addEventListener("onload", nextSequence)
-
-var randomSeq;
-var btnClrs = ["red", "blue", "green", "yellow"];
-var choosenColor = btnClrs[nextSequence()];
-
-var gamePattern = [];
-gamePattern.push(choosenColor);
-
-var userClickedPattern = [];
-
-
-function nextSequence() {
-
-    randomSeq = Math.floor(Math.random() * 4);
-    return randomSeq;
+//audio import
+{
+    var sdb = new Audio("sounds/blue.mp3");
+    var sdg = new Audio("sounds/green.mp3");
+    var sdr = new Audio("sounds/red.mp3");
+    var sdy = new Audio("sounds/yellow.mp3");
+    var sdwrong = new Audio("sounds/wrong.mp3");
 }
-
-$("#" + choosenColor).addClass("pressed")
-setTimeout(() => {
-    $("#" + choosenColor).removeClass("pressed")
-}, 100);
-
-//sounds core
-
-var sdb = new Audio("sounds/blue.mp3");
-var sdg = new Audio("sounds/green.mp3");
-var sdr = new Audio("sounds/red.mp3");
-var sdy = new Audio("sounds/yellow.mp3");
-var sdwrong = new Audio("sounds/wrong.mp3");
+//user selection code
+var userClickedPattern = []; //to memorize user colors
+var userChosenColour; //to get color wich user clicked
 //get buttons clicked
-var userChosenColour;
+
 $(".btn").on("click", function() {
 
+    nextSequence()
     userChosenColour = this.id;
     userClickedPattern.push(userChosenColour);
-
     switch (userChosenColour) {
         case "blue":
             sdb.play();
@@ -52,11 +33,40 @@ $(".btn").on("click", function() {
             sdwrong.play();
             break;
     }
-
-
+    return userChosenColour;
 });
+
+
+
+var randomSeq; //for storing random bt 0 and 3
+var btnClrs = ["red", "blue", "green", "yellow"];
+var choosenColor; //after random a nb put it value as the variabe 
+var gamePattern = []; //to memorize colors from choosenColor
+
+//select a random number
+function nextSequence() {
+    randomSeq = Math.floor(Math.random() * 4);
+    choosenColor = btnClrs[randomSeq];
+    gamePattern.push(choosenColor);
+
+    $("#" + choosenColor).click(function() {
+        $("#" + choosenColor).addClass("pressed")
+        setTimeout(() => {
+            $("#" + choosenColor).removeClass("pressed")
+        }, 100);
+    })
+    return randomSeq;
+
+}
+//animation to the btn
+//nned to put in a fnction?
+
+
+
+
+
 
 /* $(".btn").click(function() {
     alert(this.id);
 }); */
-console.log(userChosenColour);
+console.log(choosenColor)
